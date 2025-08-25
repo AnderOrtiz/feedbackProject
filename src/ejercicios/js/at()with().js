07e35912dd0508133eece35b303796c0be8a7508
Anderson Ortiz <ortizone620@gmail.com>
2025-08-24 22:03:55 -0600

    Method at(), with()

---
+++ src/ejercicios/js/at()with().js
@@ -0,0 +1,8 @@
+const 
+    arr = [1, 2, 3, 4, 5],
+    atIndex = arr.at(-2),
+    newArr = arr.with(2,22);
+
+console.log({atIndex}); // la forma mas elegante para buscar un elemento
+console.log(arr);
+console.log(newArr);
